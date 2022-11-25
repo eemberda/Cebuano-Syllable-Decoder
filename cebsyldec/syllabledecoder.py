00d39800f03822e7f7ef062cb662ac44924f7597
Eric <eric.emberda@gmail.com>
2022-11-25 13:56:15 +0800

    fixed issue with consonant clusters and ng

--- cebsyldec/syllabledecoder.py
+++ cebsyldec/syllabledecoder.py
@@ -18,7 +18,7 @@ def get_CV_sequence(word):
             elif prev_cons and char in vowels:
                 cv_seq +="V"
             elif prev_cons and prev_cons + char in consonant_clusters:
-                cv_seq +="CC"
+                cv_seq +="C"
                 prev_cons=None
             elif char in consonants:
                 cv_seq +="C"
@@ -27,6 +27,7 @@ def get_CV_sequence(word):
                 cv_seq +="V"
                 prev_cons=None
             
+            # print(char,":",cv_seq)
         
     return cv_seq
 
@@ -40,6 +41,10 @@ def get_syllables(word):
     for w in words:
         syl_seq = get_CV_sequence(w)
     
+        while "VCV" in syl_seq:
+            syl_seq = syl_seq.replace("VCV","V-CV")
+        while "VV" in syl_seq:
+            syl_seq = syl_seq.replace("VV","V-V")
         while "CCVCCV" in syl_seq:
             syl_seq = syl_seq.replace("CCVCCV","CCVC-CV")
         while "CCVCV" in syl_seq:
@@ -48,10 +53,6 @@ def get_syllables(word):
             syl_seq = syl_seq.replace("VCC","VC-C")
         while "CVCV" in syl_seq:
             syl_seq = syl_seq.replace("CVCV","CV-CV")
-        while "VV" in syl_seq:
-            syl_seq = syl_seq.replace("VV","V-V")
-        while "VCVC" in syl_seq:
-            syl_seq = syl_seq.replace("VCVC","V-CVC")
         while "VVC" in syl_seq:
             syl_seq = syl_seq.replace("VVC","V-VC")
 
@@ -69,7 +70,7 @@ def get_syllables(word):
             else:
                 syllables.append(w[i:i+chars])
                 i += chars
-                
+
     return [syllables,sequences]
 
 if __name__ == "__main__":
