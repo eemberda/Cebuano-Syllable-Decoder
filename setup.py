05ecbda932fc603bbe4704fb9a97937c6d440fc7
Eric John Emberda <eric.emberda@gmail.com>
2026-09-04 20:49:57 +0800

    Bump version to 0.0.5 (0.0.4 was already used in 2022)

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
   name = 'cebsyldec',
   packages = find_packages(),
-  version = '0.0.4',
+  version = '0.0.5',
   description = "A Cebuano Syllabicator",
   long_description = long_description,
   long_description_content_type = "text/markdown",
   author = 'Eric John Emberda',
   author_email = 'eric.emberda@gmail.com',
   url = 'https://github.com/eemberda/Cebuano-Syllable-Decoder',
-  download_url = 'https://github.com/eemberda/Cebuano-Syllable-Decoder/archive/0.0.4.tar.gz',
+  download_url = 'https://github.com/eemberda/Cebuano-Syllable-Decoder/archive/0.0.5.tar.gz',
   keywords = ['syllabicator', 'cebuano', 'bisaya', 'syllabification'],
   classifiers = [
     'Programming Language :: Python :: 3',
